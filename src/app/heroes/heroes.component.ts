642327528ce127664fa61a36e934dcc261c13a9a
kennja05 <jacobkenny05@gmail.com>
2020-11-10 15:28:43 -0500

    gen h list. display list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,13 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1, 
-    name: "Windstorm"
-  }
+  heroes = HEROES
 
-  constructor() { }
+  //lines 16-19 are auto generated, but seem to create
+  //issues with using the list iterator *ngFor
+  // constructor() { }
 
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void {
+  // }
    
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Poopoo' },
+    { id: 13, name: 'Charmander' },
+    { id: 14, name: 'Batman' },
+    { id: 15, name: 'Megamind' },
+    { id: 16, name: 'The Child' },
+    { id: 17, name: 'Tony Soprano' },
+    { id: 18, name: 'Hank Schrader' },
+    { id: 19, name: 'Gimli' },
+]
